68835058924fc66077e6351604419dc205bb8430
yuta-2v <yut.a.rumin@gmail.com>
2019-01-29 23:36:37 +0900

    Func remove user from group
    Change #user-search-result

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,10 +1,12 @@
 $(function(){
+  var $search_result = $("#user-search-result");
+
   function appendUsers(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
                 </div>`;
-    $('#user-search-result').append(html);
+    $search_result.append(html);
   }
 
   function appendUser(user){
@@ -12,7 +14,7 @@ $(function(){
                   ${user}
                 </div>
     `;
-    $('#user-search-result').append(html);
+    $search_result.append(html);
   }
 
   function addUser(id, name){
@@ -22,7 +24,7 @@ $(function(){
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>
     `;
-    $('#user-search-result').append(html);
+    $search_result.append(html);
   }
 
   $('#user-search-field').keyup(function(){
@@ -34,7 +36,7 @@ $(function(){
       dataType: 'json'
     })
     .always(function(){
-      $('#user-search-result').empty();
+      $search_result.empty();
     })
     .done(function(users){
       if (users.length !== 0){
@@ -50,10 +52,14 @@ $(function(){
     })
   });
 
-  $("#user-search-result").on('click', ".user-search-add ", function(){
+  $($search_result).on('click', ".user-search-add", function(){
     var id = $(this).attr("data-user-id")
     var name = $(this).attr("data-user-name")
     var html = addUser(id,name)
+    $(this).parent().remove();
+  });
+
+  $($search_result).on('click', ".user-search-remove", function(){
     $(this).parent().remove();
   });
 });
