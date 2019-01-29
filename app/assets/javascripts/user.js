38ce2be5e84a84da80c7abfa87969e1872d5548d
yuta-2v <yut.a.rumin@gmail.com>
2019-01-29 17:26:57 +0900

    Add view in js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,26 @@
 $(function(){
   function appendUsers(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">
+                    ${user.name}
+                  </p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`;
+    $('#user-search-result').append(html);
+    console.log("append");
+  }
+
+  function appendUser(user){
+    var html = `<div class="chat-group-user__name">
+                  ${user}
+                </div>
+    `;
+    $('#user-search-result').append(html);
+    console.log("no append");
   }
 
   $('#user-search-field').keyup(function(){
-    var input =$("#user-search-field").val();
+    var input = $.trim($("#user-search-field").val());
     console.log(input);
     $.ajax({
       url: '/users',
@@ -11,19 +28,22 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
-    .done(function(users){
-      console.log("ok");
+    .always(function(){
       $('#user-search-result').empty();
+    })
+    .done(function(users){
+      console.log(users);
       if (users.length !== 0){
+        console.log(users.length);
         users.forEach(function(user){
           appendUsers(user);
         });
       } else {
-        $('#user-search-result').empty();
-        appendUsers('nobody');
+        console.log('fault');
+        appendUser('nobody');
       }
     })
-    .fail(function(users){
+    .fail(function(){
       console.log("no");
     })
   })
