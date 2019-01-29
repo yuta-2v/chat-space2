04d39dc6f4215c34d200e48734ce6102dfb2e259
yuta-2v <yut.a.rumin@gmail.com>
2019-01-29 16:09:32 +0900

    Add Ajax done fail

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,30 @@
 $(function(){
+  function appendUsers(user){
+  }
+
   $('#user-search-field').keyup(function(){
-    console.log("fafa");
+    var input =$("#user-search-field").val();
+    console.log(input);
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      console.log("ok");
+      $('#user-search-result').empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUsers(user);
+        });
+      } else {
+        $('#user-search-result').empty();
+        appendUsers('nobody');
+      }
+    })
+    .fail(function(users){
+      console.log("no");
+    })
   })
 });
